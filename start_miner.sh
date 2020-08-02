fdfaa5eafd94eb482f40d098435958bc55709d30
mody199610 <mohamed145620@bue.edu.eg>
2020-08-02 18:57:12 +0200

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+PhoenixMiner -coin etc -pool etc.2miners.com:1010 -rvram 1 -wal 0x0263dcEE837bC9694298eEf8f814817d2f187762.1
